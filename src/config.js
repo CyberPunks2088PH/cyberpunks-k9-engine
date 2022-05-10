eb7366abafa51ca990b5880fdaa9cdd3616f80c1
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-05-10 18:46:33 +0800

    Configure trait layers

--- src/config.js
+++ src/config.js
@@ -24,15 +24,15 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 10,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Breed" },
+      { name: "Collar" },
+      { name: "Eyes" },
+      { name: "Face Mark" },
+      { name: "Head" },
+      { name: "Mouth" },
     ],
   },
 ];
@@ -42,8 +42,8 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 840,
+  height: 840,
   smoothing: false,
 };
 
